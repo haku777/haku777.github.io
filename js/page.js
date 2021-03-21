9c3c8862bc86c12480a30dbd8fd514eefc7e8268
haku <jimmy1076667239@gmail.com>
2021-03-21 11:28:52 -0500

    ajsute login se agregan parametros alos coponentes

---
+++ js/page.js
@@ -0,0 +1,27 @@
+// auto invicaciones en js: 
+
+(function(){ }()); 
+
+(function(){ })(); 
+
+(() =>{ })(); 
+
+
+((e) => {
+    console.log('permanece activo');
+
+    swal.fire(
+        'aqui se agregaran las tareas a  realizar tambien implantar alertas por campana',
+        '<i>Pdt: permanece activo</i>',
+        'question'
+        );
+
+
+})();
+
+
+
+
+
+
+
