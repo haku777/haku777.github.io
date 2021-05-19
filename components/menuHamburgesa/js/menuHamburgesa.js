b56af3010a13524da0ec32983911ff76884cc537
haku <jimmy1076667239@gmail.com>
2021-05-18 20:35:25 -0500

    ajuste general v4

--- components/menuHamburgesa/js/menuHamburgesa.js
+++ components/menuHamburgesa/js/menuHamburgesa.js
@@ -1,19 +1,9 @@
 
 const iconMenu = document.querySelector('#icono-menu'),
-    menu = document.querySelector('#menu');
+        menu = document.querySelector('#menu');
 
-    iconMenu.addEventListener('click', (e) => {
-
-
+iconMenu.addEventListener('click', (e) => {
+    
         menu.classList.toggle('active');
-        iconMenu.removeAttribute("class");
-        iconMenu.innerHTML=(src="<i class='fas fa-bars' id='icono-menu' style='font-size: 70px;'></i>");
-        document.body.classList.toggle('opacity');
-
-
-    });
-
-
-
-
-
+        menu.classList.toggle('opacity');
+});
