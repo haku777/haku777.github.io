ee29dbb2fd38e9dbe9493d342017e4c142b1aaaf
haku <jimmy1076667239@gmail.com>
2021-08-12 13:02:32 -0500

    actualizacion new portfolio y se agrega tematics

--- components/portfolio/js/controlProyects.js
+++ components/portfolio/js/controlProyects.js
@@ -3,16 +3,16 @@ console.log("agregado correctamente");
 
 let preview = document.querySelector("#preview");
 let proyect1 = document.querySelector("#proyect1").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/examples/weather.png" alt="">`;
+    preview.innerHTML = `<img src="../../img/examples/weather.PNG" alt="">`;
 });
 let proyect2 = document.querySelector("#proyect2").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/examples/search_image.png" alt="">`;
+    preview.innerHTML = `<img src="../../img/examples/search_image.PNG" alt="">`;
 });
 let proyect3 = document.querySelector("#proyect3").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/examples/properties.png" alt="">`;
+    preview.innerHTML = `<img src="../../img/examples/properties.PNG" alt="">`;
 });
 let proyect4 = document.querySelector("#proyect4").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/examples/parallax.png" alt="">`;
+    preview.innerHTML = `<img src="../../img/examples/parallax.PNG" alt="">`;
 });
 let proyect5 = document.querySelector("#proyect5").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
@@ -21,7 +21,7 @@ let proyect6 = document.querySelector("#proyect6").addEventListener("mousemove",
     preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
 });
 let proyect7 = document.querySelector("#proyect7").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/examples/burger.png" alt="">`;
+    preview.innerHTML = `<img src="../../img/examples/burger.PNG" alt="">`;
 });
 let proyect8 = document.querySelector("#proyect8").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
