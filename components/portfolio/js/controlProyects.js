82a15c3ff17fc7e18aeff45e72cd0535e9d3e0ed
haku <jimmy1076667239@gmail.com>
2021-08-18 12:40:01 -0500

    actualizacion portfolio exmples and proyects

--- components/portfolio/js/controlProyects.js
+++ components/portfolio/js/controlProyects.js
@@ -3,33 +3,33 @@ console.log("agregado correctamente");
 
 let preview = document.querySelector("#preview");
 let proyect1 = document.querySelector("#proyect1").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/examples/weather.PNG" alt="">`;
-});
-let proyect2 = document.querySelector("#proyect2").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/search_image.PNG" alt="">`;
 });
-let proyect3 = document.querySelector("#proyect3").addEventListener("mousemove", function(){
+let proyect2 = document.querySelector("#proyect2").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/properties.PNG" alt="">`;
 });
-let proyect4 = document.querySelector("#proyect4").addEventListener("mousemove", function(){
+let proyect3 = document.querySelector("#proyect3").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/parallax.PNG" alt="">`;
 });
+let proyect4 = document.querySelector("#proyect4").addEventListener("mousemove", function(){
+    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
+});
 let proyect5 = document.querySelector("#proyect5").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
+    preview.innerHTML = `<img src="../../img/examples/weather.PNG" alt="">`;
 });
 let proyect6 = document.querySelector("#proyect6").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
+    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
 });
 let proyect7 = document.querySelector("#proyect7").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/burger.PNG" alt="">`;
 });
 let proyect8 = document.querySelector("#proyect8").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
+    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
 });
 let proyect9 = document.querySelector("#proyect9").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
+    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
 });
 let proyect10 = document.querySelector("#proyect10").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/no_copiriht/bridge.jpg" alt="">`;
+    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
 });
 
---
+++ components/pruebas/php/login.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+    function conectar(){
+        throw new Exception("Fallo de conexion");
+        $con = new Mysqli("localhost","root","","preubas");
+        return $con;
+    }
+
+    try {
+        //code...
+        conectar();
+        
+    } catch (Exception $ex) {
+        //throw $th;
+        echo "Error de:   " . $ex->getMessage() . "<br>";
+    } finally {
+        echo "catch de conexion";
+    }
+
+
+?>    
+
+<h2>hola</h2>
+
+
+
+
+</body>
+</html>
