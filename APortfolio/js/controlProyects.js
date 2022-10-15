02dcc92382e35a293f4cf92f880a5e101b229e81
jimmy <jgutierrezb@evolution-it.com.co>
2022-10-15 07:09:33 -0500

    redireccion

--- APortfolio/js/controlProyects.js
+++ APortfolio/js/controlProyects.js
@@ -12,7 +12,7 @@ let proyect3 = document.querySelector("#proyect3").addEventListener("mousemove",
     preview.innerHTML = `<img src="../../img/examples/parallax.PNG" alt="">`;
 });
 let proyect4 = document.querySelector("#proyect4").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
+    preview.innerHTML = `<img src="../../img/srry.PNG" alt="">`;
 });
 let proyect5 = document.querySelector("#proyect5").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/weather.PNG" alt="">`;
@@ -32,4 +32,3 @@ let proyect9 = document.querySelector("#proyect9").addEventListener("mousemove",
 let proyect10 = document.querySelector("#proyect10").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
 });
-
