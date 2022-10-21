a37fc05ed996b566ff5000ec30910c33ae54fe6f
jimmy <jgutierrezb@evolution-it.com.co>
2022-10-21 00:49:06 -0500

    redirec change and images location

--- APortfolio/js/controlProyects.js
+++ APortfolio/js/controlProyects.js
@@ -8,27 +8,30 @@ let proyect1 = document.querySelector("#proyect1").addEventListener("mousemove",
 let proyect2 = document.querySelector("#proyect2").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/properties.PNG" alt="">`;
 });
+let proyect2_1 = document.querySelector("#proyect2_1").addEventListener("mousemove", function(){
+    preview.innerHTML = `<img src="../../img/examples/proyecto2_1.jpg" alt="">`;
+});
 let proyect3 = document.querySelector("#proyect3").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/parallax.PNG" alt="">`;
 });
 let proyect4 = document.querySelector("#proyect4").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../img/develop.jpg" alt="">`;
+    preview.innerHTML = `<img src="../img/examples/develop.jpg" alt="">`;
 });
 let proyect5 = document.querySelector("#proyect5").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/weather.PNG" alt="">`;
 });
 let proyect6 = document.querySelector("#proyect6").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
+    preview.innerHTML = `<img src="../img/examples/develop.jpg" alt="">`;
 });
 let proyect7 = document.querySelector("#proyect7").addEventListener("mousemove", function(){
     preview.innerHTML = `<img src="../../img/examples/burger.PNG" alt="">`;
 });
 let proyect8 = document.querySelector("#proyect8").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
+    preview.innerHTML = `<img src="../img/examples/develop.jpg" alt="">`;
 });
 let proyect9 = document.querySelector("#proyect9").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
+    preview.innerHTML = `<img src="../img/examples/develop.jpg" alt="">`;
 });
 let proyect10 = document.querySelector("#proyect10").addEventListener("mousemove", function(){
-    preview.innerHTML = `<img src="../../img/error2.PNG" alt="">`;
-});
+    preview.innerHTML = `<img src="../img/examples/develop.jpg" alt="">`;
+}); 
