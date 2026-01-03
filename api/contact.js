ce69d14fed263d39533b72b1452a1be60fd909d4
jimmy <jimmy1076667239@gmail.com>
2026-01-03 12:59:56 -0500

    update to conection variable no able to read

--- api/contact.js
+++ api/contact.js
@@ -6,24 +6,14 @@ export default async function handler(req, res) {
     return res.status(405).send('Metodo no permitido');
   }
 
-  // Neon nos da una función directa para conectar con la URL de conexión
-  // La URL la pegas desde tu panel de Neon (Connection String)
-  const sql = neon('postgresql://neondb_owner:npg_NUzu8F4KSToW@ep-steep-hat-ad3atyfi-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require');
+  const sql = neon(process.env.DATABASE_URL);
 
   try {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-
-    if (!name || !email) {
-        throw new Error("Faltan campos: name o email están vacíos");
-    }
-
+    const { name, email, message } = req.body;
     await sql('INSERT INTO contact (name, email, message) VALUES ($1, $2, $3)', [name, email, message]);
 
     return res.redirect(303, '/index.html?success=true');
   } catch (error) {
-    // ESTO ES CLAVE: Ahora el error te dirá EXACTAMENTE qué pasó
     return res.status(500).json({ 
         error: 'Error de base de datos', 
         details: error.message 
